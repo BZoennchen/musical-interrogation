1d64be54d6facf50c8777b5fab9b42b222a68ff4
BZoennchen <benedikt.zoennchen@web.de>
2023-06-07 16:52:26 +0200

    add utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+# the following is to play music on google colab
+from music21 import *
+from midi2audio import FluidSynth
+from IPython.display import Audio
+
+def score_to_wav(score, filename):
+    mf = midi.translate.streamToMidiFile(score)
+    mf.open('music.mid', 'wb')
+    mf.write()
+    mf.close()
+    # Convert midi to audio
+    FluidSynth().midi_to_audio('music.mid', filename)
+
+    # Play the audio file
+    return filename
